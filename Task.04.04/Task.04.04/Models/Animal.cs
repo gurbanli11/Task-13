444435f24009161fb6ae0c289731003f12c25b09
gurbanli11 <orkhan.80@bk.ru>
2024-04-04 19:02:58 +0400

    create task

---
+++ Task.04.04/Task.04.04/Models/Animal.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task._04._04.Models
+{
+    internal abstract class Animal
+    {
+        public abstract void Eat();
+    }
+    abstract class Bird : Animal
+    {
+        public abstract void Fly();
+        
+
+        
+    }
+    class Parriot : Bird
+    {
+
+        public override void Eat() 
+        {
+            Console.WriteLine("Qush yemi");
+        }
+        public void Talk()
+        {
+            Console.WriteLine("Cik cik");
+        }
+        public override void Fly()
+        {
+            Console.WriteLine("Fly is parriot");
+        }
+    }
+    class Eagle : Bird
+    {
+        public override void Eat() 
+        {
+            Console.WriteLine("Ilan yeyir");
+        }
+        public override void Fly()
+        {
+            Console.WriteLine("Fly is eagle");
+        }
+    }
+    abstract class Fish : Animal
+    {
+        public void Swim()
+        {
+            Console.WriteLine("Swim");
+        }
+    }
+    class Shark : Fish
+    {
+
+        public override void Eat()
+        {
+            Console.WriteLine("Kicik baliqlari yeyir");
+        }
+        public void Hunt()
+        {
+            Console.WriteLine("Dead");
+        }
+    }
+}
---
+++ Task.04.04/Task.04.04/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Reflection.Metadata;
+using Task._04._04.Models;
+
+namespace Task._04._04
+{
+
+    internal record StudentRecord()
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; } 
+        public byte Age { get; set; }   
+        public string Group { get; set; }   
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parriot ali = new Parriot();    
+            Shark acula = new Shark();
+            Eagle sahin = new Eagle();  
+            //ali.Eat();
+            //acula.Eat();
+            //sahin.Fly();
+            //ali.Fly();
+            Animal[] animals = {ali, acula,sahin};
+            foreach (Animal item in animals)
+            {
+                item.Eat();
+            }
+            
+            //StudentRecord st1 = new StudentRecord()
+            //{
+            //    Name = "Orkhan",
+            //    Surname = "Gurbanli",
+            //    Age = 20,
+            //    Group="AB106"
+            //};
+            //StudentRecord st2 = new StudentRecord()
+            //{
+            //    Name = "James",
+            //    Surname = "Bond",
+            //     Age = 20,
+            //    Group = "AB106"
+            //};
+            //Console.WriteLine(st1 == st2);
+
+
+            //(int, string) A = (1, "Salam");
+            //   Console.WriteLine(A.Item1);
+
+
+            //(string, string, int) B = ("Orxan","Qurbanli",20);
+            //Console.WriteLine(B);
+  
+
+            //(int, string,string,bool) C = (2, "Code","Academy",true);
+            //Console.WriteLine(C);
+        }
+    }
+
+    
+}
